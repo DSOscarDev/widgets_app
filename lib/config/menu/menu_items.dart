3a8010b33456f83fa3ec5a1d864adfa7c5412c6f
oscar <oclemente.dscont@gmail.com>
2023-12-24 22:15:13 -0500

    snackbars termindado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -32,10 +32,16 @@ const appMenuItems =<MenuItem>[
   ),
 
   MenuItem(
-  title: 'ProgressIndicators',
+  title: 'Progress Indicators',
   subTitle: 'Generales y controlados',
   link: '/progress',
   icon: Icons.refresh_rounded
+  ),
+  MenuItem(
+  title: 'SanckBars y dialogos',
+  subTitle: 'Indicadores en pantalla',
+  link: '/snackbars',
+  icon: Icons.info_outline
   ),
   
 ];
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
 
@@ -6,8 +7,75 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackBar(BuildContext context){
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackbar = SnackBar(
+      content: const Text('Hola Mundo'),
+      action: SnackBarAction(label: 'ok!',onPressed: () {}),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+
+  }
+
+  void openDialog(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('estas seguro?'),
+        content: const Text('parrafo descriptivo'),
+        actions: [
+          TextButton(onPressed: ()=> context.pop(), child: const Text('Cancelar')),
+          FilledButton(onPressed: ()=> context.pop(), child: const Text('Aceptar'))
+          
+        ],
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Snackbars y Dialogos"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context, 
+                  children: [
+                    const Text('descripcion del Diaologo Oscar')
+                  ]
+                );
+              },
+              child: const Text('Licencias Usadas')
+            ),
+            FilledButton.tonal(
+              onPressed: ()=>openDialog(context),
+              child: const Text('mostrar Diálogo')
+            )            
+
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text("Mostrar SnackBars"),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        // onPressed: (){
+        //   showCustomSnackBar(context);
+        // },
+        onPressed: ()=> showCustomSnackBar(context),
+      ),
+    );
   }
 }
