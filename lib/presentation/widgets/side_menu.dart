31f926528a17134f57519a428588dd546b84cf37
oscar <oclemente.dscont@gmail.com>
2023-12-29 00:47:28 -0500

    navigator ohoto

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -11,56 +11,45 @@ class SideMenu extends StatefulWidget {
 }
 
 class _SideMenuState extends State<SideMenu> {
-
   int navDrawerIndex = 1;
 
   @override
   Widget build(BuildContext context) {
-
-    final hasNotch = MediaQuery.of(context).viewPadding.top>35; // para verificar si se tiene un notch en la cabecera de la pantalla sobre todo en Ios
-    
+    final hasNotch = MediaQuery.of(context).viewPadding.top >
+        35; // para verificar si se tiene un notch en la cabecera de la pantalla sobre todo en Ios
 
     return NavigationDrawer(
-      selectedIndex: navDrawerIndex,
-      onDestinationSelected: (value) {
-        setState(() {
-          navDrawerIndex=value;
-        });
-      },
-      children: [
-        Padding(
-          padding: EdgeInsets.fromLTRB(28, hasNotch?10:20, 16, 10),
-          child: const Text('Main'),
-        ),
-        ...appMenuItems
-        .sublist(0,3)
-        .map((item) => NavigationDrawerDestination(
-            icon: Icon(item.icon),
-            label: Text(item.title)
-          )
-        ),
-
-        const Padding(
-          padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
-          child: Divider(),
-        ),
-
-        const Padding(
-          padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
-          child: Text('More Options'),
-        ),
-
-        ...appMenuItems
-        .sublist(3)
-        .map((item) => NavigationDrawerDestination(
-            icon: Icon(item.icon),
-            label: Text(item.title)
-          )
-        ),
-
-
-
-      ]
-    );
+        selectedIndex: navDrawerIndex,
+        onDestinationSelected: (value) {
+          setState(() {
+            navDrawerIndex = value;
+          });
+        },
+        children: [
+          UserAccountsDrawerHeader(
+              accountName: const Text('Oscar Clemente'),
+              accountEmail: const Text('oscarpremiun@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                child: ClipOval(child: Image.asset('assets/images/1.png')),
+              ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(28, hasNotch ? 10 : 20, 16, 10),
+            child: const Text('Main'),
+          ),
+          ...appMenuItems.sublist(0, 3).map((item) =>
+              NavigationDrawerDestination(
+                  icon: Icon(item.icon), label: Text(item.title))),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+            child: Divider(),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+            child: Text('More Options'),
+          ),
+          ...appMenuItems.sublist(3).map((item) => NavigationDrawerDestination(
+              icon: Icon(item.icon), label: Text(item.title))),
+        ]);
   }
 }
